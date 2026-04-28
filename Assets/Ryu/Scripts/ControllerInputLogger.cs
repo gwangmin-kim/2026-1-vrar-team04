48b5738d02165b70dc8ecfc6e02e752b6a1a33ea
enggaa <enggaa@snu.ac.kr>
2026-04-28 15:39:18 +0900

    design: fix for code convention

--- Assets/Ryu/Scripts/ControllerInputLogger.cs
+++ Assets/Ryu/Scripts/ControllerInputLogger.cs
@@ -1,15 +1,16 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class ControllerInputLogger : MonoBehaviour
 {
-    [SerializeField] private InputActionAsset xriActions;
+    [SerializeField] private InputActionAsset _xriActions;
 
-    private InputAction leftPrimary;
-    private InputAction leftSecondary;
-    private InputAction rightPrimary;
-    private InputAction rightSecondary;
-    private InputAction leftMenu;
+    private InputAction _leftPrimary;
+    private InputAction _leftSecondary;
+    private InputAction _rightPrimary;
+    private InputAction _rightSecondary;
+    private InputAction _leftMenu;
 
     void OnEnable()
     {
@@ -18,43 +19,47 @@ public class ControllerInputLogger : MonoBehaviour
         Bind("XRI Right Interaction", "Select", "Right Grip");
         Bind("XRI Right Interaction", "Activate", "Right Trigger");
 
-        xriActions?.Enable();
+        _xriActions?.Enable();
 
-        leftPrimary = new InputAction(binding: "<XRController>{LeftHand}/{PrimaryButton}");
-        leftSecondary = new InputAction(binding: "<XRController>{LeftHand}/{SecondaryButton}");
-        rightPrimary = new InputAction(binding: "<XRController>{RightHand}/{PrimaryButton}");
-        rightSecondary = new InputAction(binding: "<XRController>{RightHand}/{SecondaryButton}");
-        leftMenu = new InputAction(binding: "<XRController>{LeftHand}/{MenuButton}");
+        _leftPrimary = new InputAction(binding: "<XRController>{LeftHand}/{PrimaryButton}");
+        _leftSecondary = new InputAction(binding: "<XRController>{LeftHand}/{SecondaryButton}");
+        _rightPrimary = new InputAction(binding: "<XRController>{RightHand}/{PrimaryButton}");
+        _rightSecondary = new InputAction(binding: "<XRController>{RightHand}/{SecondaryButton}");
+        _leftMenu = new InputAction(binding: "<XRController>{LeftHand}/{MenuButton}");
 
-        leftPrimary.performed += _ => Debug.Log("[Controller] Left Primary (X) pressed");
-        leftSecondary.performed += _ => Debug.Log("[Controller] Left Secondary (Y) pressed");
-        rightPrimary.performed += _ => Debug.Log("[Controller] Right Primary (A) pressed");
-        rightSecondary.performed += _ => Debug.Log("[Controller] Right Secondary (B) pressed");
-        leftMenu.performed += _ => Debug.Log("[Controller] Left Menu pressed");
+        _leftPrimary.performed += _ => Debug.Log("[Controller] Left Primary (X) pressed");
+        _leftSecondary.performed += _ => Debug.Log("[Controller] Left Secondary (Y) pressed");
+        _rightPrimary.performed += _ => Debug.Log("[Controller] Right Primary (A) pressed");
+        _rightSecondary.performed += _ => Debug.Log("[Controller] Right Secondary (B) pressed");
+        _leftMenu.performed += _ => Debug.Log("[Controller] Left Menu pressed");
 
-        leftPrimary.Enable();
-        leftSecondary.Enable();
-        rightPrimary.Enable();
-        rightSecondary.Enable();
-        leftMenu.Enable();
+        _leftPrimary.Enable();
+        _leftSecondary.Enable();
+        _rightPrimary.Enable();
+        _rightSecondary.Enable();
+        _leftMenu.Enable();
     }
 
     void OnDisable()
     {
-        xriActions?.Disable();
-        leftPrimary?.Disable();
-        leftSecondary?.Disable();
-        rightPrimary?.Disable();
-        rightSecondary?.Disable();
-        leftMenu?.Disable();
+        _xriActions?.Disable();
+        _leftPrimary?.Disable();
+        _leftSecondary?.Disable();
+        _rightPrimary?.Disable();
+        _rightSecondary?.Disable();
+        _leftMenu?.Disable();
     }
 
     void Bind(string mapName, string actionName, string logName)
     {
-        var action = xriActions?.FindAction($"{mapName}/{actionName}");
+        var action = _xriActions?.FindAction($"{mapName}/{actionName}");
         if (action != null)
+        {
             action.performed += _ => Debug.Log($"[Controller] {logName} pressed");
+        }
         else
+        {
             Debug.LogWarning($"[Controller] Action not found: {mapName}/{actionName}");
+        }
     }
 }
