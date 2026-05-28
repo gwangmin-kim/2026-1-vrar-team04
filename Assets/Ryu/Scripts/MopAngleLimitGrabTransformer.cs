ef74c364d09e08219a6106039175b7677a0f1756
enggaa <enggaa@snu.ac.kr>
2026-05-29 01:32:52 +0900

    fix: mop control

--- Assets/Ryu/Scripts/MopAngleLimitGrabTransformer.cs
+++ Assets/Ryu/Scripts/MopAngleLimitGrabTransformer.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 using UnityEngine.XR.Interaction.Toolkit.Transformers;
 
 [AddComponentMenu("XR/Transformers/Mop Angle Limit Grab Transformer (VR&AR Team 04)")]
 public class MopAngleLimitGrabTransformer : XRGeneralGrabTransformer
 {
+    [Header("Grab Point Gating")]
+    [SerializeField]
+    private bool _limitOnlyWhenLowerGripIsHeld = true;
+
+    [SerializeField]
+    private Transform _upperGrip;
+
+    [SerializeField]
+    private Transform _lowerGrip;
+
     [Header("Angle Limits From Grab Start")]
     [SerializeField]
     private bool _limitPitch = true;
@@ -30,11 +41,13 @@ public class MopAngleLimitGrabTransformer : XRGeneralGrabTransformer
     private float _maxRotationDegreesPerSecond = 540f;
 
     private Quaternion _grabStartRotation;
+    private bool _wasLowerGripHeld;
 
     public override void OnGrab(XRGrabInteractable grabInteractable)
     {
         base.OnGrab(grabInteractable);
         _grabStartRotation = grabInteractable.transform.rotation;
+        _wasLowerGripHeld = false;
     }
 
     public override void Process(
@@ -44,6 +57,19 @@ public class MopAngleLimitGrabTransformer : XRGeneralGrabTransformer
         ref Vector3 localScale)
     {
         base.Process(grabInteractable, updatePhase, ref targetPose, ref localScale);
+
+        bool lowerGripHeld = !_limitOnlyWhenLowerGripIsHeld || IsLowerGripHeld(grabInteractable);
+        if (!lowerGripHeld)
+        {
+            _wasLowerGripHeld = false;
+            return;
+        }
+
+        if (!_wasLowerGripHeld)
+        {
+            _grabStartRotation = grabInteractable.transform.rotation;
+            _wasLowerGripHeld = true;
+        }
 
         Quaternion targetDelta = Quaternion.Inverse(_grabStartRotation) * targetPose.rotation;
         Vector3 targetEuler = targetDelta.eulerAngles;
@@ -66,6 +92,37 @@ public class MopAngleLimitGrabTransformer : XRGeneralGrabTransformer
         }
 
         targetPose.rotation = limitedRotation;
+    }
+
+    private bool IsLowerGripHeld(XRGrabInteractable grabInteractable)
+    {
+        Transform upperGrip = _upperGrip != null ? _upperGrip : grabInteractable.attachTransform;
+        Transform lowerGrip = _lowerGrip != null ? _lowerGrip : grabInteractable.secondaryAttachTransform;
+
+        if (lowerGrip == null)
+            return true;
+
+        if (upperGrip == null)
+            return true;
+
+        foreach (var interactor in grabInteractable.interactorsSelecting)
+        {
+            if (interactor == null)
+                continue;
+
+            Transform interactorAttach = interactor.GetAttachTransform(grabInteractable);
+            Vector3 interactorPosition = interactorAttach != null
+                ? interactorAttach.position
+                : interactor.transform.position;
+
+            float upperDistance = Vector3.SqrMagnitude(interactorPosition - upperGrip.position);
+            float lowerDistance = Vector3.SqrMagnitude(interactorPosition - lowerGrip.position);
+
+            if (lowerDistance <= upperDistance)
+                return true;
+        }
+
+        return false;
     }
 
     private static float ClampSignedAngle(float angle, float maxAbsAngle)
